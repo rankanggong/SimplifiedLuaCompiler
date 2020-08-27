dcad512562e1c6180e8a723509d60db9fd6dd0e4
rankanggong <54407936+rankanggong@users.noreply.github.com>
2020-08-27 09:45:20 +0800

    Add files via upload

---
+++ Interpreter/func/toNumber.java
@@ -0,0 +1,37 @@
+package interpreter.built_ins;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import interpreter.JavaFunction;
+import interpreter.LuaInt;
+import interpreter.LuaString;
+import interpreter.LuaValue;
+import interpreter.ASTVisitorAdapter.TypeException;
+
+public class toNumber extends JavaFunction {
+
+	@Override
+	public List<LuaValue> call(List<LuaValue> functionargs) throws TypeException {
+		LuaValue arg = functionargs.get(0);
+		List<LuaValue> retList = new ArrayList<>();
+		if (arg instanceof LuaInt) { 
+			retList.add(arg);
+			return retList;
+		}
+		if (arg instanceof LuaString) {
+			int num = Integer.parseInt(((LuaString)arg).value);
+			retList.add(new LuaInt(num));
+			return retList;
+		}
+		throw new TypeException("Cannot convert " + arg + "to a number");
+	}
+
+	@Override
+	public LuaValue copy() {
+		return this;
+	}
+
+
+
+}
